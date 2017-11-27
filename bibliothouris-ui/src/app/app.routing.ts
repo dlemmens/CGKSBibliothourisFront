0c57a15bc8ee8eac2e0cda4ffd6b7f3d61bf5126
iamleka <len.karpisek@gmail.com>
2017-11-27 15:31:17 +0100

    frontendzever

--- bibliothouris-ui/src/app/app.routing.ts
+++ bibliothouris-ui/src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './app.guard';
-import {Users} from './user/users.component';
+import { Users } from './user/users.component';
+// import { Books } from './user/users.component';
 
 const appRoutes: Routes = [
-    { path: '', component: Users },
+    { path: 'users', component: Users }
+    // { path: 'books', component: Books }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
